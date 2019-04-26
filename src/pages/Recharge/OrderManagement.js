a98d14b6fd37a460c98e2b9ab45d6fe777ece7d2
Jiapan_Yu <2541771811@qq.com>
2019-04-26 10:35:50 +0800

    修复bug EMS-36

--- src/pages/Recharge/OrderManagement.js
+++ src/pages/Recharge/OrderManagement.js
@@ -145,7 +145,7 @@ class OrderManagement extends Component {
     const { dispatch, form } = this.props;
 
     form.setFieldsValue({
-      'userName': form.getFieldValue('userId') && form.getFieldValue('userId').trim(),
+      'userName': form.getFieldValue('userName') && form.getFieldValue('userName').trim(),
       'iccardId': form.getFieldValue('iccardId') && form.getFieldValue('iccardId').trim(),
       'iccardIdentifier': form.getFieldValue('iccardIdentifier') && form.getFieldValue('iccardIdentifier').trim(),
       'invoiceCode': form.getFieldValue('invoiceCode') && form.getFieldValue('invoiceCode').trim(),
